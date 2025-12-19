641bc1bd3052e078b5fd7ac967d83eb6a41bf100
Cervac Petru <petru.cervac@gmail.com>
2025-12-19 20:35:36 +0200

    ci: sign assembly

--- sign.ps1
+++ sign.ps1
@@ -1,4 +1,4 @@
 $cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject 'CN=Revit Addin Dev' -CertStoreLocation Cert:\CurrentUser\My
-$cert | Export-Certificate -FilePath dev-root.cer
-Import-Certificate -FilePath .\dev-root.cer -CertStoreLocation Cert:\CurrentUser\Root | Format-Table
-Set-AuthenticodeSignature -FilePath .\bin\Release\net10.0\signing-playground.dll -Certificate $cert | Format-Table
+# $cert | Export-Certificate -FilePath dev-root.cer
+# Import-Certificate -FilePath .\dev-root.cer -CertStoreLocation Cert:\CurrentUser\Root | Format-Table
+Set-AuthenticodeSignature -FilePath .\Sample\bin\Release\net10.0\Sample.dll -Certificate $cert | Format-Table
