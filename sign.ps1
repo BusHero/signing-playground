86b1f4cb67b8d2010b290e009cf25631205f2a2c
Cervac Petru <petru.cervac@gmail.com>
2025-12-19 23:42:41 +0200

    ci: gets certificate from file

--- sign.ps1
+++ sign.ps1
@@ -1,4 +1,8 @@
-$cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject 'CN=Revit Addin Dev' -CertStoreLocation Cert:\CurrentUser\My
+# $cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject 'CN=Revit Addin Dev' -CertStoreLocation Cert:\CurrentUser\My
 # $cert | Export-Certificate -FilePath dev-root.cer
 # Import-Certificate -FilePath .\dev-root.cer -CertStoreLocation Cert:\CurrentUser\Root | Format-Table
+# Export-PfxCertificate -Cert $cert -FilePath cert.pfx -Password $mypwd
+
+$mypwd = ConvertTo-SecureString -String '1234' -Force -AsPlainText
+$cert = Get-PfxCertificate -FilePath .\cert.pfx -Password $mypwd
 Set-AuthenticodeSignature -FilePath .\Sample\bin\Release\net10.0\Sample.dll -Certificate $cert | Format-Table
